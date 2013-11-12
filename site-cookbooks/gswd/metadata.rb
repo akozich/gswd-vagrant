fbd32060f09d0118a67c03e160054fd69d1d8568
Aliaksei Kozich <alex.kozich@gmail.com>
2013-11-12 14:43:05 +0300

    Merge with master

--- site-cookbooks/gswd/metadata.rb
+++ site-cookbooks/gswd/metadata.rb
@@ -1,3 +1,4 @@
+name              "gswd"
 maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
@@ -5,5 +6,8 @@ description       "Customizations for the GSWD development VM"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 
-depends "postgresql"
-depends "database"
+depends           "postgresql"
+depends           "database"
+
+recipe            "gswd", "Installs virtualenv for project."
+recipe            "gswd::database", "Installs database for project."
